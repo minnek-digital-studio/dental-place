9f1c8845823051d2e1c3e2bd522aec38756475dd
Anthuan Vasquez <anthuan@minnekdigital.com>
2024-02-19 14:24:48 -0400

    chore: DP-1 add required templates files
    also add base styles for layout, pages, comments and posts. add multiple theme support features and other function template tags

--- themes/minnek-theme/footer.php
+++ themes/minnek-theme/footer.php
@@ -1,11 +1,38 @@
-        </div><!-- .site-main -->
-    </div><!-- #content -->
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package mnk
+ */
+?>
+            </div><!-- .content-wrap -->
+        </div><!-- #content -->
 
-    <footer id="footer" class="site-footer">
-        <div id="copyrights">
-            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-        </div>
-    </footer>
+        <footer id="footer" class="site-footer">
+            <div class="site-info">
+                <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mnk' ) ); ?>">
+                    <?php
+                        /* translators: %s: CMS name, i.e. WordPress. */
+                        printf( esc_html__( 'Proudly powered by %s', 'mnk' ), 'WordPress' );
+                    ?>
+                </a>
+
+                <span class="sep"> | </span>
+
+                <?php
+                    printf( esc_html__( 'Theme Development by %1$s.', 'mnk' ), '<a href="https://minnekdigital.com/">Minnek</a>' );
+                ?>
+            </div><!-- .site-info -->
+
+            <div id="copyrights">
+                <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+            </div>
+        </footer>
+    </div><!-- #wrapper -->
 
     <?php wp_footer(); ?>
 </body>
--- themes/minnek-theme/header.php
+++ themes/minnek-theme/header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package _s
+ * @package mnk
  */
 ?>
 <!doctype html>
@@ -20,22 +20,62 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <a class="skip-link screen-reader-text" href="#primary">
-        <?php esc_html_e( 'Saltar al contenido', 'mnk' ); ?>
-    </a>
 
-    <header id="header" class="site-header">
-        <nav id="menu" class="main-navigation">
-            <?php
-            // Display the navigation menu
-            wp_nav_menu(array(
-                'theme_location' => 'primary-menu',
-                'menu_id'        => 'primary-menu',
-                'container' => 'ul',
-            ));
-            ?>
-        </nav>
-    </header>
+    <?php wp_body_open(); ?>
 
-    <div id="content">
-        <div class="site-main">
+    <div id="wrapper" class="site">
+        <a class="skip-link screen-reader-text" href="#content">
+            <?php esc_html_e( 'Skip to content', 'mnk' ); ?>
+        </a>
+
+        <header id="header" class="site-header">
+            <div class="site-branding">
+                <?php
+                the_custom_logo();
+
+                if ( is_front_page() && is_home() ) :
+                    ?>
+                    <h1 class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <?php bloginfo( 'name' ); ?>
+                        </a>
+                    </h1>
+                    <?php
+                else :
+                    ?>
+                    <p class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            <?php bloginfo( 'name' ); ?>
+                        </a>
+                    </p>
+                    <?php
+                endif;
+
+                $mnk_description = get_bloginfo( 'description', 'display' );
+
+                if ( $mnk_description || is_customize_preview() ) :
+                    ?>
+                    <p class="site-description">
+                        <?php echo $mnk_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                    </p>
+                <?php endif; ?>
+            </div><!-- .site-branding -->
+
+            <nav id="menu" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                    <?php esc_html_e( 'Primary Menu', 'mnk' ); ?>
+                </button>
+
+                <?php
+                // Display the navigation menu
+                wp_nav_menu(array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                    'container'      => 'ul',
+                ));
+                ?>
+            </nav>
+        </header>
+
+        <div id="content">
+            <div class="site-content">
