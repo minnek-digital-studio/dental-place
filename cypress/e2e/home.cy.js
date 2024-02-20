a34e2df52dbec0a61e7c7eac5f2985fd60183ebf
Anthuan Vasquez <anthuan@minnekdigital.com>
2024-02-20 09:27:53 -0400

    chore: DP-1 add cypress eslint config
    also add the default screenshot for the theme and missing nav script content

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,11 +1,11 @@
 const targetUrl = '/';
 
-describe('Check homepage', () => {
-    before(() => {
-        cy.visit('/');
-    });
+describe( 'Check homepage', () => {
+    before( () => {
+        cy.visit( '/' );
+    } );
 
-    it('should visit and load the homepage', () => {
-        cy.url().should('include', targetUrl);
-    });
-});
+    it( 'should visit and load the homepage', () => {
+        cy.url().should( 'include', targetUrl );
+    } );
+} );
