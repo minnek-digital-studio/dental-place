cc30762360cf5dfc5af4058ce46f80ee7e4bb958
Joan Corona <joan@minnekdigital.com>
2024-07-04 15:17:23 -0400

    chore(ui): DP-18 add inputs components

--- apps/web/pages/components/index.tsx
+++ apps/web/pages/components/index.tsx
@@ -1,14 +1,85 @@
 
 import { Input } from "@minnek/ui/components/input";
-import { Button } from "@minnek/ui/components/button";
+import { Textarea } from "@minnek/ui/components/textarea";
+import { Label } from "@minnek/ui/components/label";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@minnek/ui/components/select";
 
 export default function Components() {
     return (
         <main className="w-full h-screen flex justify-center items-center">
-            <div>
-                <h1>Inputs</h1>
-                <Input type="text" placeholder="Input" />
-                <Button>Button</Button>
+            <div className="flex gap-40">
+                <section className="flex flex-col gap-6">
+                    <h3>Inputs</h3>
+                    <Label>
+                        <span>Name</span>
+                        <Input type="text" placeholder="Input" />
+                    </Label>
+                    <Label>
+                        <span>Name</span>
+                        <Input type="text" variant="info" sizes="lg" placeholder="Input" />
+                    </Label>
+                </section>
+
+                <section className="flex flex-col gap-6">
+                    <h3>Select</h3>
+                    <Label>
+                        <span>Fruits</span>
+                        <Select>
+                            <SelectTrigger sizes="lg" variant="info">
+                                <SelectValue placeholder="Select a fruit" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Fruits</SelectLabel>
+                                    <SelectItem value="apple">Apple</SelectItem>
+                                    <SelectItem value="banana">Banana</SelectItem>
+                                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                                    <SelectItem value="grapes">Grapes</SelectItem>
+                                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Label>
+                    <Label>
+                        <span>Fruits</span>
+                        <Select>
+                            <SelectTrigger >
+                                <SelectValue placeholder="Select a fruit" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Fruits</SelectLabel>
+                                    <SelectItem value="apple">Apple</SelectItem>
+                                    <SelectItem value="banana">Banana</SelectItem>
+                                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                                    <SelectItem value="grapes">Grapes</SelectItem>
+                                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Label>
+                </section>
+
+                <section className="flex flex-col gap-6">
+                    <h3>Textarea</h3>
+                    <Label>
+                        <span>Message</span>
+                        <Textarea placeholder="Message" />
+                    </Label>
+                    <Label>
+                        <span>Message</span>
+                        <Textarea variant="info" sizes="lg" placeholder="Message" />
+                    </Label>
+                </section>
+
             </div>
         </main>
     );
--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -1,19 +1,40 @@
 import * as React from "react"
 
 import { cn } from "../lib/utils"
+import { cva, type VariantProps } from "class-variance-authority";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  const inputVariants = cva(
+    "flex w-full rounded-full border border-input bg-input text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 font-normal",
+    {
+      variants: {
+        variant: {
+          default: "placeholder:text-muted-foreground bg-input",
+          info: "placeholder:text-muted-foreground bg-info",
+        },
+        sizes: {
+          default: "h-10 px-4 py-2",
+          lg: "h-11 px-5 py-6",
+          icon: "h-10 w-10",
+        },
+      },
+      defaultVariants: {
+        variant: "default",
+        sizes: "default",
+      },
+    }
+  )
+
+  export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, variant, sizes, type, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
+        className={cn(inputVariants({ variant, sizes, className }))}
         ref={ref}
         {...props}
       />
