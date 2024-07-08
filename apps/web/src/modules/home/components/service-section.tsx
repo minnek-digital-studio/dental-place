e93088e4b5608cad4d481133784b9633835cd943
Joan Corona <joan@minnekdigital.com>
2024-07-08 15:52:06 -0400

    chore(app:web): DP-5 make responsive services section

--- apps/web/src/modules/home/components/service-section.tsx
+++ apps/web/src/modules/home/components/service-section.tsx
@@ -24,12 +24,12 @@ const ServiceSection = ({
     button,
 }: ServiceSectionProps) => {
     return (
-        <section className="flex flex-col gap-12 items-center py-6 px-4">
+        <section className="flex flex-col gap-12 items-center py-6">
             <header className="max-w-[30rem] flex flex-col gap-6 text-center">
                 <Typography as="h2">{title}</Typography>
                 <Typography as="p">{description}</Typography>
             </header>
-            <main className="flex w-full justify-center gap-2">
+            <main className="flex w-full flex-col md:flex-row justify-center items-center gap-2">
                 {items.map((item) => (
                     <ServiceCard key={crypto.randomUUID()} {...item} />
                 ))}
