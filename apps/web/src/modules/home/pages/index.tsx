739e43a6522f0b9c55f8376223d726cff6621c29
Joan Corona <joaneliasc86@gmail.com>
2024-07-11 10:56:35 -0400

    feat(app:web): DP-13 add ars section

--- apps/web/src/modules/home/pages/index.tsx
+++ apps/web/src/modules/home/pages/index.tsx
@@ -9,34 +9,20 @@ import { CMS_NAME } from "@/lib/constants";
 import ServiceSection, {
     ServiceSectionProps,
 } from "../components/services-section";
+import ArsSection, { ArsSectionProps } from "../components/ars-section";
 import Services from "../data/services.json";
+import ArsInfo from "../data/ars.json";
 
 export const metadata: Metadata = {
     title: `Next.js Blog Example with ${CMS_NAME}`,
 };
 
 export async function HomePage({ preview = false }) {
-    const { edges } = await getAllPostsForHome(preview);
-    const heroPost = edges[0]?.node;
-    const morePosts = edges.slice(1);
-
     return (
         <Layout preview={preview}>
             <Container>
-                <Intro />
-                {heroPost && (
-                    <HeroPost
-                        title={heroPost.title}
-                        coverImage={heroPost.featuredImage}
-                        date={heroPost.date}
-                        author={heroPost.author}
-                        slug={heroPost.slug}
-                        excerpt={heroPost.excerpt}
-                    />
-                )}
-                {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-
                 <ServiceSection {...(Services as ServiceSectionProps)} />
+                <ArsSection {...(ArsInfo as ArsSectionProps)} />
             </Container>
         </Layout>
     );
