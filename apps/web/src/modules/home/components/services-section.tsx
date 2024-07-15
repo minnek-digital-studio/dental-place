6b8484b9fec2f60f20c62960cef190d506d44ea0
Joan Corona <joan@minnekdigital.com>
2024-07-15 16:46:32 -0400

    feat(app:web): DP-5 reduce gap of items & more padding x

--- apps/web/src/modules/home/components/services-section.tsx
+++ apps/web/src/modules/home/components/services-section.tsx
@@ -26,12 +26,12 @@ const ServiceSection = ({
     button,
 }: ServiceSectionProps) => {
     return (
-        <section className="flex flex-col gap-12 items-center py-6">
+        <section className="flex flex-col gap-9 items-center py-6">
             <header className="max-w-[30rem] flex flex-col gap-6 text-center">
                 <Typography as="h2">{title}</Typography>
                 <Typography as="p">{description}</Typography>
             </header>
-            <main className="flex w-full flex-col md:flex-row justify-center items-center gap-2 md:flex-wrap">
+            <main className="md:flex md:w-full md:flex-row justify-center items-center gap-2 md:flex-wrap grid grid-cols-1 sm:grid-cols-2">
                 {items.map((item) => (
                     <ServiceCard key={crypto.randomUUID()} {...item} />
                 ))}
