5f715f28e0143955d46344cf2d95c02b6b561f1c
Joan Corona <joan@minnekdigital.com>
2024-07-17 16:55:20 -0400

    feat(app:web): DP-9 add reviews section

---
+++ apps/web/src/modules/home/components/review-card.tsx
@@ -0,0 +1,61 @@
+import { type Author } from "@/modules/common/types";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from "@minnek/ui/components/card";
+import { Comma } from "@minnek/ui/icons";
+import { cn } from "@minnek/ui/lib/utils";
+import { Typography } from "@minnek/ui/components/typography";
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback,
+} from "@minnek/ui/components/avatar";
+import StarRating from "@/modules/common/components/rating-stars";
+
+export interface ReviewCardProps extends React.HTMLAttributes<HTMLDivElement> {
+    comment: string;
+    author: Author;
+    rating: number;
+}
+
+const ReviewCard = ({
+    comment,
+    author,
+    rating,
+    className,
+    ...props
+}: ReviewCardProps) => {
+    return (
+        <Card className={cn("max-w-[14.5rem] w-full bg-[#F9F7FF]", className)}>
+            <CardHeader className="px-4 mb-2">
+                <StarRating rating={rating} />
+            </CardHeader>
+            <CardContent className="px-4">
+                <Typography
+                    as="p"
+                    className="line-clamp-[8] text-sm text-black"
+                >
+                    {comment}
+                </Typography>
+            </CardContent>
+            <CardFooter className="flex px-4 gap-4 justify-between">
+                <Avatar>
+                    <AvatarImage {...author.img} />
+                    <AvatarFallback>{author.name[0]}</AvatarFallback>
+                </Avatar>
+                <Typography as="h4" className="text-base">
+                    {author.name}
+                </Typography>
+                <div className="flex flex-1 justify-end -space-x-2 -mx-2">
+                    <Comma size={25} />
+                    <Comma size={25} />
+                </div>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default ReviewCard;
