d62d1ee9e8875311d88f2e71aca8ebcc05b5769a
Joan Corona <joan@minnekdigital.com>
2024-07-18 13:26:26 -0400

    chore(app:web): DP-8 add container class into section

--- apps/web/src/modules/home/components/whyus-section.tsx
+++ apps/web/src/modules/home/components/whyus-section.tsx
@@ -27,40 +27,47 @@ const WhyUsSection = ({
     className,
 }: WhyUsSectionProps) => {
     return (
-        <section
-            className={cn("flex flex-col md:flex-row bg-primary/40", className)}
-        >
-            <header className="flex flex-col gap-6 w-full md:w-[60%] p-10 md:py-14 md:pr-10">
-                <Typography as="h3">{title}</Typography>
-                <ul className="flex flex-col gap-6">
-                    {items.map((item, index) => (
-                        <li key={index} className="flex gap-4">
-                            <div>
-                                <IconByName {...item.icon} />
-                            </div>
-                            <div className="flex flex-col">
-                                <Typography as="h4">{item.title}</Typography>
-                                <Typography as="p">
-                                    {item.description}
-                                </Typography>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                <Typography as="a" href={button.link} className="font-bold">
-                    <Button
-                        variant={button.variant}
-                        className="gap-2"
-                        size={button.size}
-                    >
-                        <span>{button.text}</span>
-                        <MoveRight size={25} />
-                    </Button>
-                </Typography>
-            </header>
-            <main className="md:w-[40%] w-full overflow-hidden p-10 py-14">
-                <Image {...img} />
-            </main>
+        <section className="mx-5">
+            <div
+                className={cn(
+                    "flex flex-col md:flex-row container pl-0 pr-0",
+                    className,
+                )}
+            >
+                <header className="flex flex-col gap-6 w-full md:w-[60%] p-10 md:py-14 md:pr-10">
+                    <Typography as="h3">{title}</Typography>
+                    <ul className="flex flex-col gap-6">
+                        {items.map((item, index) => (
+                            <li key={index} className="flex gap-4">
+                                <div>
+                                    <IconByName {...item.icon} />
+                                </div>
+                                <div className="flex flex-col">
+                                    <Typography as="h4">
+                                        {item.title}
+                                    </Typography>
+                                    <Typography as="p">
+                                        {item.description}
+                                    </Typography>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <Typography as="a" href={button.link} className="font-bold">
+                        <Button
+                            variant={button.variant}
+                            className="gap-2"
+                            size={button.size}
+                        >
+                            <span>{button.text}</span>
+                            <MoveRight size={25} />
+                        </Button>
+                    </Typography>
+                </header>
+                <main className="md:w-[40%] w-full overflow-hidden p-10 py-14">
+                    <Image {...img} />
+                </main>
+            </div>
         </section>
     );
 };
