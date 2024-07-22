08189947fb0ace850266b76d0bc7096f63a849b5
Joan Corona <joan@minnekdigital.com>
2024-07-22 17:02:14 -0400

    chore(app:web): DP-40 align with other section on desktop

--- apps/web/src/modules/services/components/clinic-section.tsx
+++ apps/web/src/modules/services/components/clinic-section.tsx
@@ -40,7 +40,7 @@ const ClinicSection = ({ title, description, items }: ClinicSectionProps) => {
                     align: "center",
                     loop: true,
                 }}
-                className="flex flex-col gap-14 w-full items-center container pl-0 pr-0 "
+                className="flex flex-col gap-14 w-full items-center container max-lg:pl-0 max-lg:pr-0 "
             >
                 <CarouselContent>
                     {items.map((item, index) => (
