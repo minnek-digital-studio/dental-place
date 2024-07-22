522196e91b747a39054087ba125fc9ffe64cbbc3
Joan Corona <joan@minnekdigital.com>
2024-07-22 16:52:59 -0400

    feat(app:web): DP-40 add carousel clinic photos section

--- apps/web/src/modules/services/pages/index.tsx
+++ apps/web/src/modules/services/pages/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "@/modules/common/layouts/layout";
 
+import ClinicSection, {
+    type ClinicSectionProps,
+} from "@/modules/services/components/clinic-section";
+import ClinicInfo from "@/modules/services/data/clinic.json";
+
 const ServicesPage = () => {
     return (
         <Layout
@@ -9,6 +14,7 @@ const ServicesPage = () => {
             }}
         >
             <h1>Services</h1>
+            <ClinicSection {...(ClinicInfo as ClinicSectionProps)} />
         </Layout>
     );
 };
