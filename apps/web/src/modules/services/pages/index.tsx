7ac37609b1c3f60a4ebb7886ca917bea2c7fda2a
Joan Corona <joan@minnekdigital.com>
2024-07-22 15:02:41 -0400

    chore(app:web): change layout logic

---
+++ apps/web/src/modules/services/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "@/modules/common/layouts/layout";
+
+const ServicesPage = () => {
+    return (
+        <Layout
+            navbarVariant={{
+                variant: "info",
+            }}
+        >
+            <h1>Services</h1>
+        </Layout>
+    );
+};
+
+export default ServicesPage;
