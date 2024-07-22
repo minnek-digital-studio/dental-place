0ad8481fcd2fe756db7e5ff0da88dcb605043b2f
Joan Corona <joan@minnekdigital.com>
2024-07-22 15:06:32 -0400

    feat(app:web): DP-41 add let us Help sections into services

--- apps/web/src/modules/services/pages/index.tsx
+++ apps/web/src/modules/services/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Layout from "@/modules/common/layouts/layout";
 
+import LetUsHelpSection, {
+    LetUsHelpSectionProps,
+} from "@/modules/common/components/lethelp-section";
+
+import LetHelpInfo from "@/modules/services/data/let-us-help.json";
+
 const ServicesPage = () => {
     return (
         <Layout
@@ -9,6 +15,7 @@ const ServicesPage = () => {
             }}
         >
             <h1>Services</h1>
+            <LetUsHelpSection {...(LetHelpInfo as LetUsHelpSectionProps)} />
         </Layout>
     );
 };
