e11a89710dad709ed037e4ed38eedbe72ee3a05e
adonyssantos <58638286+adonyssantos@users.noreply.github.com>
2024-07-24 11:19:24 -0400

    feat: DP-26 add contact page hero

--- apps/web/src/modules/contact_us/pages/index.tsx
+++ apps/web/src/modules/contact_us/pages/index.tsx
@@ -8,6 +8,8 @@ import { Button } from "@minnek/ui/components/button";
 import { Card, CardContent, CardHeader } from "@minnek/ui/components/card";
 import { Typography } from "@minnek/ui/components/typography";
 import { IconByName } from "@minnek/ui/components/icons/index";
+import { NavBar } from "@/modules/common/components/navbar";
+import Footer from "@/modules/common/components/footer";
 
 const ContactCard = () => {
     return (
@@ -29,72 +31,77 @@ const ContactCard = () => {
 
 const ServicesPage = () => {
     return (
-        <Layout
-        // navbarVariant={{
-        //     variant: "info",
-        // }}
-        >
-            <h1>Contáctanos</h1>
-            <section className="container max-w-[1430px] mb-20">
-                {/* <section className="container max-xl:pl-0 max-xl:pr-0"> */}
-                {/* two cols, one left with the map and another on right with a form */}
-                <div className={cn(
-                    "flex flex-col md:flex-row h-full bg-white px-10 py-8 gap-14 shadow-lg rounded-3xl",
-                )}>
-                    <header className="block w-full h-[216px] md:w-[380px] md:h-[648px] rounded-3xl bg-red-300"></header>
-                    <form className="flex flex-col w-full h-full md:h-[745px] gap-11">
-                        <div className="flex gap-6 flex-col md:flex-row">
+        <>
+            <div
+                className="h-[400px] bg-cover bg-center bg-no-repeat"
+                style={{
+                    backgroundImage: `url('/images/contact-us-hero.webp')`,
+                }}>
+                <NavBar variant="transparent" />
+                <h1 className="text-white font-bold text-5xl md:text-6xl text-center mt-16 md:mt-20">Contáctanos</h1>
+            </div>
+
+            <div className="absolute w-full -translate-y-36 md:-translate-y-28">
+                <section className="container max-w-[1430px] mb-20">
+                    <div className={cn(
+                        "flex flex-col md:flex-row h-full bg-white px-10 py-8 gap-14 shadow-lg rounded-3xl",
+                    )}>
+                        <header className="block w-full h-[216px] md:w-[380px] md:h-[648px] rounded-3xl bg-red-300"></header>
+                        <form className="flex flex-col w-full h-full md:h-[745px] gap-11">
+                            <div className="flex gap-6 flex-col md:flex-row">
+                                <Label className="w-full">
+                                    <span className="text-2xl font-bold">Tu nombre</span>
+                                    <Input
+                                        type="text"
+                                        variant="info"
+                                        sizes="lg"
+                                        placeholder="Tu nombre"
+                                    />
+                                </Label>
+                                <Label className="w-full">
+                                    <span className="text-2xl font-bold">Tu Email</span>
+                                    <Input
+                                        type="text"
+                                        variant="info"
+                                        sizes="lg"
+                                        placeholder="Tu Email"
+                                    />
+                                </Label>
+                            </div>
                             <Label className="w-full">
-                                <span className="text-2xl font-bold">Tu nombre</span>
+                                <span className="text-2xl font-bold">Número de teléfono</span>
                                 <Input
                                     type="text"
                                     variant="info"
                                     sizes="lg"
-                                    placeholder="Tu nombre"
+                                    placeholder="Número de teléfono"
                                 />
                             </Label>
-                            <Label className="w-full">
-                                <span className="text-2xl font-bold">Tu Email</span>
-                                <Input
-                                    type="text"
+                            <Label>
+                                <span className="text-2xl font-bold">Escribenos aquí...</span>
+                                <Textarea
+                                    className="rounded-3xl h-[314px] resize-none"
                                     variant="info"
                                     sizes="lg"
-                                    placeholder="Tu Email"
+                                    placeholder="Escribenos aquí..."
                                 />
                             </Label>
-                        </div>
-                        <Label className="w-full">
-                            <span className="text-2xl font-bold">Número de teléfono</span>
-                            <Input
-                                type="text"
-                                variant="info"
-                                sizes="lg"
-                                placeholder="Número de teléfono"
-                            />
-                        </Label>
-                        <Label>
-                            <span className="text-2xl font-bold">Escribenos aquí...</span>
-                            <Textarea
-                                className="rounded-3xl h-[314px] resize-none"
-                                variant="info"
-                                sizes="lg"
-                                placeholder="Escribenos aquí..."
-                            />
-                        </Label>
-                        <div className="w-full flex justify-end">
-                            <Button variant="dark" className="w-fit py-6 px-14">Enviar</Button>
-                        </div>
-                    </form>
-                </div>
-            </section>
-            <section className="container max-w-[1430px] mb-20">
-                <div className="flex flex-col md:flex-row gap-6">
-                    <ContactCard />
-                    <ContactCard />
-                    <ContactCard />
-                </div>
-            </section>
-        </Layout>
+                            <div className="w-full flex justify-end">
+                                <Button variant="dark" className="w-fit py-6 px-14">Enviar</Button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+                <section className="container max-w-[1430px] mb-20">
+                    <div className="flex flex-col md:flex-row gap-6">
+                        <ContactCard />
+                        <ContactCard />
+                        <ContactCard />
+                    </div>
+                </section>
+                <Footer />
+            </div>
+        </>
     );
 };
 
