46dea2be597cc86fc64b2faec03434245c090e1f
Ramon Jimenez <ramon@minnekdigital.com>
2024-07-30 17:14:21 -0400

    feat: creating the section 'What Has Been Done'

---
+++ apps/web/src/modules/case_studies/components/been-done.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Typography } from "@minnek/ui/components/typography";
+import { cn } from "@minnek/ui/lib/utils";
+import { Image } from "@/modules/common/components/image";
+import { Icon, ImageType } from "@/modules/common/types";
+import { IconByName } from "@minnek/ui/components/icons/index";
+export interface BeenDoneSectionProps
+    extends React.HTMLAttributes<HTMLDivElement> {
+    title: string;
+    description?: string;
+    itemsIcons: BeenDoneItem[];
+}
+
+export type BeenDoneItem = {
+    title: string;
+    description?: string;
+    icon: Icon;
+};
+
+const BeenDoneSection = ({
+    title,
+    description,
+    itemsIcons,
+    className,
+    ...props
+}: BeenDoneSectionProps) => {
+    let descriptionHTML = { __html: "" };
+    if (description) descriptionHTML = { __html: description };
+
+    return (
+        <section
+            className={cn(
+                "bg-white py-10 px-5 md:px-10 lg:px-20",
+                className,
+            )}
+            {...props}
+        >
+            <main className="max-w-[47rem] md:max-w-[55rem] flex flex-col gap-3 text-center container">
+                <Typography
+                    as="h2"
+                    className="leading-[3rem] lg:leading-[3rem] text-left text-xl lg:text-2xl"
+                >
+                    {title}
+                </Typography>
+                {description && (
+                    <Typography 
+                        as="p" 
+                        className="text-dark-grey text-justify leading-[1.6rem]"
+                        dangerouslySetInnerHTML={descriptionHTML}
+                    />
+                )}
+                <ul className="flex flex-col gap-4 grid md:grid-cols-2 mt-3">
+                    {itemsIcons.map((item, index) => (
+                        <li key={index} className="flex gap-4 items-center">
+                            <div className="bg-[#28AF77] rounded-full size-5 md:size-6 flex items-center justify-center">
+                                <IconByName {...item.icon} />
+                            </div>
+                            <div className="flex flex-col gap-1">
+                                <Typography
+                                    as="h3"
+                                    className="text-xl lg:text-xl font-extrabold tracking-normal"
+                                >
+                                    {item.title}
+                                </Typography>
+                                {item.description && (
+                                    <Typography
+                                        as="p"
+                                        className="leading-6"
+                                    >
+                                        {item.description}
+                                    </Typography>
+                                )}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+        </section>
+    );
+};
+
+export default BeenDoneSection;
---
+++ apps/web/src/modules/case_studies/pages/slug.tsx
@@ -0,0 +1,38 @@
+import Layout from "@/modules/common/layouts/layout";
+import { cn } from "@minnek/ui/lib/utils";
+import { Typography } from "@minnek/ui/components/typography";
+import Footer from "@/modules/common/components/footer";
+import { Metadata } from "next";
+import BeenDoneSection, {
+    BeenDoneSectionProps,
+} from "../components/been-done";
+import BeenDoneInfo from "@/modules/case_studies/data/been-done.json";
+
+export const metadata: Metadata = {
+    title: "Case Studies",
+    description: "Meet the team that makes Dental Place possible",
+};
+
+const CaseStudiesPage = ({ params }) => {
+    const beenDone = BeenDoneInfo.items.find(
+        (item) => item.slug === params.slug,
+    );
+
+    if (!beenDone) {
+        return "Case study not found";
+    }
+
+    return (
+        <Layout
+            navbarVariant={{
+                variant: "light-primary",
+            }}
+        >
+            <BeenDoneSection
+                {...(beenDone as BeenDoneSectionProps)}
+            />
+        </Layout>
+    );
+};
+
+export default CaseStudiesPage;
