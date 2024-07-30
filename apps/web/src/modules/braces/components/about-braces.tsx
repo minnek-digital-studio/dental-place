3ff3d4fdaebb03fde4a492b203f6273869233d4c
Joan Corona <joan@minnekdigital.com>
2024-07-30 14:55:36 -0400

    chore(app:web): DP-34 remove unnecesary space

--- apps/web/src/modules/braces/components/about-braces.tsx
+++ apps/web/src/modules/braces/components/about-braces.tsx
@@ -33,16 +33,16 @@ const AboutBracesSection = ({
     if (description) descriptionHTML = { __html: description };
 
     return (
-        <section className={cn("", className)}>
+        <section className={cn("md:py-14", className)}>
             <div
                 className={cn(
-                    "grid grid-cols-1 md:grid-cols-2 gap-16 container py-10 md:py-14",
+                    "grid grid-cols-1 md:grid-cols-2 gap-16 container",
                 )}
             >
                 <picture className="max-md:max-w-sm max-md:m-auto md:w-auto h-full max-h-[33rem] max-md:order-2">
                     <Image {...img} />
                 </picture>
-                <main className="flex flex-col gap-6 w-full pt-10 md:py-14">
+                <main className="flex flex-col gap-6 w-full md:pt-10 md:py-14">
                     <Typography as="h2" className="font-extrabold">
                         {title}
                     </Typography>
