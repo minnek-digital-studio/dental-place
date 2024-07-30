2de4f0e0cbcf331d0a64b61ed123b333eb2729f5
Joan Corona <joan@minnekdigital.com>
2024-07-30 08:46:31 -0400

    chore(app:web): add page braces config

---
+++ apps/web/src/modules/braces/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "@/modules/common/layouts/layout";
+
+const ExpressBracesPage = async () => {
+    return (
+        <Layout
+            navbarVariant={{
+                variant: "light-primary",
+            }}
+        >
+            <h1>Hola</h1>
+        </Layout>
+    );
+};
+
+export default ExpressBracesPage;
