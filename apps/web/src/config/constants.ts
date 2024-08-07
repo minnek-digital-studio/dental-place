bde487b963ecf324358e0e22fcf475f882777f17
Joan Corona <joan@minnekdigital.com>
2024-08-07 09:54:38 -0400

    chore(app:web): DP-54 add contact-form action example

---
+++ apps/web/src/config/constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: process.env.WORDPRESS_API_URL || '',
+    WORDPRESS_CONTACT_FORM_URL: process.env.WORDPRESS_CONTACT_FORM_URL || '',
+    WPCF7_TAG: process.env.WPCF7_TAG || '',
+};
--- apps/web/src/modules/common/lib/apollo/apollo-client.ts
+++ apps/web/src/modules/common/lib/apollo/apollo-client.ts
@@ -1,4 +1,4 @@
-import { config } from "../config";
+import { config } from "@/config/constants";
 import { HttpLink } from "@apollo/client";
 import {
   registerApolloClient,
