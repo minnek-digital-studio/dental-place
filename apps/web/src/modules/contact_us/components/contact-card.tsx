0e781b7e7355236f4f753d04153c92229e211ef0
Joan Corona <joan@minnekdigital.com>
2024-08-08 13:10:05 -0400

    chore(app:web): DP-68 change size of container & move map on mobile

--- apps/web/src/modules/contact_us/components/contact-card.tsx
+++ apps/web/src/modules/contact_us/components/contact-card.tsx
@@ -10,14 +10,17 @@ export interface ContactCardProps {
 
 const ContactCard = ({ title, children, iconName }: ContactCardProps) => {
     return (
-        <Card className="flex flex-row shadow-none min-h-28">
+        <Card className="flex flex-row shadow-none w-max">
             <CardHeader className="p-0 mr-4">
                 <div className="flex justify-center items-center w-14 h-14 rounded-full bg-primary">
                     <IconByName name={iconName} size={28} />
                 </div>
             </CardHeader>
-            <CardContent className="px-0">
-                <Typography as="h3" className="text-xl lg:text-xl my-2">
+            <CardContent className="px-0 flex flex-col">
+                <Typography
+                    as="h3"
+                    className="text-xl lg:text-xl my-2 lg:text-nowrap"
+                >
                     {title}
                 </Typography>
                 {children}
