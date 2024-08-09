062cbf472643d47068643a65999989d9130b922b
Joan Corona <joan@minnekdigital.com>
2024-08-09 13:41:20 -0400

    chore: DP-54 add codegen dev script

--- apps/web/codegen.dev.ts
+++ apps/web/codegen.dev.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http:localhost:8080/graphql",
+  schema: "http://localhost:8080/graphql/",
   documents: "src/**/*.{gql,graphql}",
   generates: {
     "src/graphql/generated/": {
