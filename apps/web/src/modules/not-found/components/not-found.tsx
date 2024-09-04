920e098d906de205d7b91e694f28a03c65f7fce9
Joan Corona <joan@minnekdigital.com>
2024-09-04 16:13:52 -0400

    chore(app:web): DP-84 move not-found page to module

---
+++ apps/web/src/modules/not-found/components/not-found.tsx
@@ -0,0 +1,34 @@
+import { Typography } from "@minnek/ui/components/typography";
+import { Button } from "@minnek/ui/components/button";
+import Link from "next/link";
+import { IconByName } from "@minnek/ui/icons";
+import { ButtonType } from "@/modules/common/types";
+
+export type NotFoundProps = {
+    title: string;
+    description: string;
+    button: ButtonType;
+    icon: ButtonType["icon"];
+};
+
+const NotFound = ({ title, description, button, icon }: NotFoundProps) => {
+    return (
+        <section className="flex flex-col w-full items-center justify-center pb-12 gap-9 text-center bg-primary">
+            <header className="flex flex-col gap-9 text-center justify-center items-center px-6">
+                <IconByName {...icon} />
+                <Typography as="h1">{title}</Typography>
+                <Typography
+                    as="p"
+                    className="text-lg md:text-lg lg:text-lg max-w-[35rem]"
+                >
+                    {description}
+                </Typography>
+            </header>
+            <Button variant={button.variant} size={button.size} asChild>
+                <Link href={button.link}>{button.text}</Link>
+            </Button>
+        </section>
+    );
+};
+
+export default NotFound;
