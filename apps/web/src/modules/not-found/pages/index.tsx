4df53c5a50ad2d6c3bbc18aa8de3c13d880f96fa
Joan Corona <joan@minnekdigital.com>
2024-09-04 16:37:26 -0400

    chore(app:web): DP-84 handler not found service

--- apps/web/src/modules/not-found/pages/index.tsx
+++ apps/web/src/modules/not-found/pages/index.tsx
@@ -1,11 +1,6 @@
 import Layout from "@/modules/common/layouts/layout";
 import { Metadata } from "next";
-import { Typography } from "@minnek/ui/components/typography";
-import { Button } from "@minnek/ui/components/button";
-import Link from "next/link";
-import { IconByName, SadTooth } from "@minnek/ui/icons";
 import NotFound, { type NotFoundProps } from "../components/not-found";
-import { ButtonType } from "@/modules/common/types";
 import NotFoundInfo from "@/modules/not-found/data/not-found.json";
 
 export const metadata: Metadata = {
