b9061beb29cbf7b827709b7a7ae230ba078ebe6c
Joan Corona <joan@minnekdigital.com>
2024-09-17 09:10:43 -0400

    ci: change release it configuration

--- packages/release/.release-it.js
+++ packages/release/.release-it.js
@@ -7,17 +7,29 @@ module.exports = {
     '@release-it/conventional-changelog': {
       path: '.',
       infile: 'CHANGELOG.md',
-      preset: 'conventionalcommits',
+      preset: {
+        name: "angular"
+      },
       gitRawCommitsOpts: {
         path: '.',
       },
+    "@release-it/bumper": {
+        "in": {
+            "file": "package.json",
+            "path": ["version"]
+        },
+      "out": {
+        "file": "package.json",
+        "path": ["version"]
+      }
+    }
     },
   },
   git: {
     push: true,
     tagName: `${packageName}-v${version}`,
     commitsPath: '.',
-    commitMessage: `chore(${scope}): release v${version} [skip ci]`,
+    commitMessage: `chore(${scope}): release v${version}`,
     requireCommits: true,
     requireCommitsFail: false,
   },
@@ -26,7 +38,7 @@ module.exports = {
     versionArgs: ['--workspaces false'],
   },
   github: {
-    release: true,
+    release: false,
     releaseName: `${packageName}-v${version}`,
   },
 };
