be0af8b478bb66fd270e8a94855848ae0d13fe40
Joan Corona <joan@minnekdigital.com>
2024-09-17 09:10:43 -0400

    ci: change release it configuration

---
+++ packages/release/.release-it.js
@@ -0,0 +1,32 @@
+const version = '${version}';
+const packageName = process.env.npm_package_name;
+const scope = packageName.split('/')[1];
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}-v${version}`,
+    commitsPath: '.',
+    commitMessage: `chore(${scope}): release v${version} [skip ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  npm: {
+    publish: false,
+    versionArgs: ['--workspaces false'],
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-v${version}`,
+  },
+};
