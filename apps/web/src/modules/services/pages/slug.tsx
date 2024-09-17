8a2e8794c04ac339cce3468c999880439b99e607
Joan Elias Corona Gómez <105407871+jcorona48@users.noreply.github.com>
2024-09-17 16:01:14 -0400

    release/DP-42 (#80)
    * feat(app:web): DP-41 add let us Help sections into services
    
    * feat(app:web): DP-39 add services section on services page
    
    * feat(app:web): DP-40 add carousel clinic photos section
    
    * chore(app:web): DP-40 align with other section on desktop
    
    * chore(app:web): DP-39 add padding bottom
    
    * chore(app:web): add margin bottom
    
    * chore(app:web): move margin apply section
    
    * chore(app:web): connect services page with wordpress
    
    * feat(app:web): DP-43 connect service & services with wordpress
    
    * chore(app:web): DP-43 solve build problem & redirect to 404
    
    * feat(app:web): DP-47 add Lets Help Section
    
    * feat(app:web): DP-46 add case studies section
    
    * feat(app:web): DP-43 add service placeholder content
    
    * ci: DP-46 change ignore files
    
    * chore(app:web): DP-46 add id for section
    
    * ci: DP-47 change ignore files
    
    * chore(app:web): DP-47 add export for service props
    
    * chore(app:web): DP-46 add more spacing & center title on mobile
    
    * chore(web): release v0.5.0
    
    * chore(ui): release v0.5.0

---
+++ apps/web/src/modules/services/pages/slug.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import type { Metadata, ResolvingMetadata } from "next";
+import { getServices, getServiceBySlug } from "../actions/services.action";
+import Layout from "@/modules/common/layouts/layout";
+import ServiceSection, {
+    type ServiceSectionProps,
+} from "../components/service-section";
+import LetUsHelpSection, {
+    type LetUsHelpSectionProps,
+} from "@/modules/common/components/lethelp-section";
+import LetUsHelpInfo from "../data/let-us-help.json";
+import { notFound } from "next/navigation";
+import CaseStudiesInfo from "../data/case-studies.json";
+import CaseStudiesSection, {
+    type CaseStudiesSectionProps,
+} from "../components/case-studies-section";
+
+type Props = {
+    params: { slug: string };
+};
+
+export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata,
+): Promise<Metadata> {
+    // read route params
+    const { slug } = params;
+
+    const service = await getServiceBySlug(slug);
+
+    return {
+        title: `${service?.title} | Dental Place`,
+        description: `${service?.description}`,
+    };
+}
+
+const ServicePage = async ({ params }) => {
+    const { slug } = params;
+    const service = await getServiceBySlug(slug);
+
+    return (
+        <Layout
+            navbarVariant={{
+                variant: "secondary",
+            }}
+        >
+            <ServiceSection {...(service as ServiceSectionProps)} />
+            <CaseStudiesSection
+                {...(CaseStudiesInfo as CaseStudiesSectionProps)}
+            />
+            <LetUsHelpSection {...(LetUsHelpInfo as LetUsHelpSectionProps)} />
+        </Layout>
+    );
+};
+
+export async function generateStaticParams() {
+    const services = await getServices();
+
+    return services.map((service) => ({
+        slug: service.slug,
+    }));
+}
+
+export default ServicePage;
